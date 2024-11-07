f784e1c0eacbc86c80ddc836a59aea046b6f2772
Najib Fikri <najibfikri13@gmail.com>
2024-11-07 11:23:07 +0700

    Add Snykk's RESTful API starter project

---
+++ cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"runtime"
+
+	"github.com/sirupsen/logrus"
+	"github.com/snykk/transaction-api/cmd/api/server"
+	"github.com/snykk/transaction-api/internal/config"
+	"github.com/snykk/transaction-api/internal/constants"
+	"github.com/snykk/transaction-api/pkg/logger"
+)
+
+func init() {
+	if err := config.InitializeAppConfig(); err != nil {
+		logger.Fatal(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryConfig})
+	}
+	logger.Info("configuration loaded", logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryConfig})
+}
+
+func main() {
+	numCPU := runtime.NumCPU()
+	logger.InfoF("The project is running on %d CPU(s)", logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryConfig}, numCPU)
+
+	if runtime.NumCPU() > 2 {
+		runtime.GOMAXPROCS(numCPU / 2)
+	}
+
+	app, err := server.NewApp()
+	if err != nil {
+		logger.Panic(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryServer})
+	}
+	if err := app.Run(); err != nil {
+		logger.Fatal(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategoryServer})
+	}
+}
---
+++ cmd/seed/seeders/seeder.users.go
@@ -0,0 +1,37 @@
+package seeders
+
+import (
+	"github.com/sirupsen/logrus"
+	"github.com/snykk/transaction-api/internal/constants"
+	"github.com/snykk/transaction-api/internal/datasources/records"
+	"github.com/snykk/transaction-api/pkg/helpers"
+	"github.com/snykk/transaction-api/pkg/logger"
+)
+
+var pass string
+var UserData []records.Users
+
+func init() {
+	var err error
+	pass, err = helpers.GenerateHash("12345")
+	if err != nil {
+		logger.Panic(err.Error(), logrus.Fields{constants.LoggerCategory: constants.LoggerCategorySeeder})
+	}
+
+	UserData = []records.Users{
+		{
+			Username: "patrick star 7",
+			Email:    "patrick@gmail.com",
+			Password: pass,
+			Active:   true,
+			RoleId:   1,
+		},
+		{
+			Username: "john doe",
+			Email:    "johndoe@gmail.com",
+			Password: pass,
+			Active:   false,
+			RoleId:   2,
+		},
+	}
+}
