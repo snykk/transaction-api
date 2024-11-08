64a31e8edf575163e56113a1ac085e148082a20c
Najib Fikri <najibfikri13@gmail.com>
2024-11-08 12:40:46 +0700

    fix: update 'transactions' table migration to handle deposit and withdraw
    - Modified 'quantity' column logic to support both purchase and non-purchase transactions:
      - If the transaction is a deposit or withdrawal (without a product), quantity can be NULL.
      - If the transaction is a purchase (with a product), quantity must be greater than 0.
    - Ensured proper handling of transaction types: 'deposit', 'withdraw', and 'purchase'.
    - Removed unused 'status' and 'description' fields from the schema for now.

--- cmd/migration/migrations/5_create_tables_transactions.up.sql
+++ cmd/migration/migrations/5_create_tables_transactions.up.sql
@@ -3,7 +3,10 @@ CREATE TABLE transactions (
     wallet_id uuid NOT NULL REFERENCES wallets(wallet_id) ON DELETE CASCADE, -- wallet terkait
     product_id INT REFERENCES products(product_id) ON DELETE SET NULL, -- produk yang dibeli, nullable untuk transaksi non-pembelian
     amount DECIMAL(15, 2) NOT NULL CHECK (amount > 0), -- jumlah uang yang ditransaksikan
-    quantity INT DEFAULT 1 CHECK (quantity > 0), -- kuantitas produk, default 1
+    quantity INT CHECK (
+        (product_id IS NULL AND quantity IS NULL) OR
+        (product_id IS NOT NULL AND quantity > 0)
+    ),
     transaction_type VARCHAR(20) NOT NULL CHECK (transaction_type IN ('deposit', 'withdraw', 'purchase')), -- tipe transaksi
     -- status VARCHAR(20) NOT NULL CHECK (status IN ('pending', 'completed', 'failed')), -- status transaksi
     -- description TEXT,
